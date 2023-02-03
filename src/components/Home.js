256ad3cd7f09e2a4de3ad8c238e5780ab6f5c0a6
davelarde <danielavelarde4@gmail.com>
2023-02-03 13:37:37 -0500

    creating components

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import { Container } from "react-bootstrap";
+ 
+
+    function Home (){
+        return (
+            <h1>Hello</h1>
+        )
+
+
+    }
+
+
+    export {Home}
