186ccb13dcd54a2293f4fb33b9d1ca5685b0e73d
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2023-02-10 13:45:23 -0800

    debugging routing and adding dummy data for recipes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,45 @@ import HomepageNavbar from './components/HomepageNavbar'
 import { Routes, Route } from 'react-router-dom'
 
 import Recipes from './components/Recipes'
+import RecipeType from './components/RecipeType.js';
+
+const DUMMY_DATA = [
+  {
+    id: 1,
+    title: 'chilaquiles',
+    type: 'breakfast',
+  },
+  {
+    id: 2,
+    title: 'overnight oats',
+    type: 'breakfast',
+  },
+  {
+    id: 3,
+    title: 'eggs benedict',
+    type: 'breakfast',
+  },
+  {
+    id: 4,
+    title: 'boring ham sandwich',
+    type: 'lunch',
+  },
+  {
+    id: 5,
+    title: 'dry meat loaf',
+    type: 'dinner',
+  },
+  {
+    id: 6,
+    title: 'steak and potatoes',
+    type: 'dinner',
+  },
+  {
+    id: 7,
+    title: 'caesar salad',
+    type: 'lunch',
+  }
+]
 
 function App() {
   return (
@@ -14,8 +53,22 @@ function App() {
 
       <HomepageNavbar />
       <Routes>
-        <Route path='' element={<Home />}/>
-        <Route path='recipes' element={<Recipes />}/>
+        <Route
+          path=''
+          element={<Home />}
+        />
+        <Route
+          path='recipes'
+          element={
+            <Recipes data={DUMMY_DATA} />
+          }
+        />
+        <Route
+          path='recipes/:recipeType'
+          element={
+            <RecipeType data={DUMMY_DATA} />
+          }
+        />
       </Routes>
     </div>
   );
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,57 +1,25 @@
 import React from 'react';
 
-import { useParams } from 'react-router-dom';
+function Recipes({ data }) {
 
-const DUMMY_DATA = [
-    {
-        id: 1,
-        title: 'chilaquiles',
-        type: 'breakfast',
-    },
-    {
-        id: 2,
-        title: 'overnight oats',
-        type: 'breakfast',
-    },
-    {
-        id: 3,
-        title: 'eggs benedict',
-        type: 'breakfast',
-    },
-    {
-        id: 4,
-        title: 'boring ham sandwich',
-        type: 'lunch',
-    },
-    {
-        id: 5,
-        title: 'dry meat loaf',
-        type: 'dinner',
-    },
-    {
-        id: 6,
-        title: 'steak and potatoes',
-        type: 'dinner',
-    },
-    {
-        id: 7,
-        title: 'caesar salad',
-        type: 'lunch',
-    }
-]
-
-function Recipes() {
     return (
-        <>
+
+
+        <div>
+
             {
-                DUMMY_DATA.map(recipe => (
+                data.map(recipe => (
                     <section key={recipe.id}>
                         <h3>{recipe.title}</h3>
                         <p>{recipe.type}</p>
-                    </section>        
+                    </section>
                 ))
             }
-        </>
+        </div>
+
+
+
+
     )
 }
 
